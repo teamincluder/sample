832387c15a94be55b3c377528e82ce8dbe704d91
232fumiya <21311232fn@tama.ac.jp>
2016-10-26 17:44:50 +0900

    当たり判定テストコード書きました

--- Assets/Scripts/Collider/Get_Hit.cs
+++ Assets/Scripts/Collider/Get_Hit.cs
@@ -1,15 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UniRx;
+using UniRx.Triggers;
 public class Get_Hit : MonoBehaviour {
+	private const string GUARD_PATH			=	"Unity/Guard";
+	private const string JAB_PATH 			=	"Unity/Jab";
+	private const string STRONG_PATH 		= 	"Unity/Strong";
+	private const string DEATH_BLOW_PATH	= 	"Unity/DeathBlow";
 
-	// Use this for initialization
+	Trigger_Hit guardfunc;
+	Trigger_Hit jabfunc;
+	Trigger_Hit strongfunc;
+	Trigger_Hit deathblowfunc;
+
+
 	void Start () {
-	
+		First_Key_List player = new First_Key_List (true);
+
+		guardfunc 		=	getTriggerHit (GUARD_PATH);
+		jabfunc 		=	getTriggerHit (JAB_PATH);
+		strongfunc		=	getTriggerHit (STRONG_PATH);
+		deathblowfunc	=	getTriggerHit (DEATH_BLOW_PATH);
+
+		/*Guard処理*/
+		this.UpdateAsObservable ()
+			.Where (_ => Input.GetKey(player.guard_Key))
+			.Where (_ => guardfunc.is_Hit)
+			.Subscribe (_ => 
+				{
+					Debug.Log("ガード");	
+				});
+
+		this.UpdateAsObservable()
+			.Where (_ => Input.GetKeyDown(player.jab_Key))
+			.Where (_ => jabfunc.is_Hit)
+			.Subscribe(_ =>
+				{
+					Debug.Log("弱攻撃");
+				});
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	private Trigger_Hit getTriggerHit(string path){
+		Trigger_Hit value = GameObject.Find (path).GetComponent<Trigger_Hit> ();
+		return value;
 	}
 }
---
+++ Assets/Scripts/Collider/Trigger_Hit.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trigger_Hit : MonoBehaviour {
+	private bool ishit = false;
+	public bool is_Hit{
+		get{
+			return ishit;
+		}
+	}
+	void OnTriggerStay2D(Collider2D collider){
+		if (collider.tag == "Player")
+			ishit = true;
+	}
+	void OnTriggerExit2D(Collider2D collider){
+		if (collider.tag == "Player")
+			ishit = false;
+	}
+}
