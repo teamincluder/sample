2770de86e06380fea8d5796287007445cf927777
232fumiya <21311232fn@tama.ac.jp>
2016-11-24 03:11:00 +0900

    anim制御

--- Assets/Scripts/Animation/Animation_Manager.cs
+++ Assets/Scripts/Animation/Animation_Manager.cs
@@ -1,3 +1,28 @@
-﻿public class Animation_Manager {
-	
+﻿using UnityEngine;
+public class Animation_Manager : MonoBehaviour{
+	private const 	string FIRST_ANIM	=	"1P/C_Man";
+
+
+	private static 	Animation_Manager instance;
+	public	static 	Animation_Manager get_Instance{
+		get{
+			return instance;
+		}
+	}
+
+	private Animation_Interface firstanim;
+	public 	Animation_Interface first{
+		get{
+			if (firstanim == null)
+				firstanim = GameObject.Find (FIRST_ANIM).GetComponent<Animation_Interface> ();
+			return firstanim;
+		}
+	}
+
+	private void Awake(){
+		if (instance == null)
+			instance = this;
+		else
+			Destroy (this.gameObject);
+	}
 }
--- Assets/Scripts/Animation/C_Man_Anim.cs
+++ Assets/Scripts/Animation/C_Man_Anim.cs
@@ -1,16 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class C_Man_Anim : MonoBehaviour {
-	private const string ANIM_DAMAGE_STR	= "C_Damage";
-	private const string ANIM_RUN_STR 		= "";
+public class C_Man_Anim : MonoBehaviour,Animation_Interface {
+	Animator anim;
 
-	Animation anim ;
-
-	void Start(){
-		anim =	this.gameObject.GetComponent<Animation>();
-		anim.Play (ANIM_DAMAGE_STR);
+	void Awake(){
+		anim =	this.gameObject.GetComponent<Animator>();
 	}
 
-
+	public void run ()
+	{
+		anim.SetTrigger ("Run");
+	}
+	public void jab ()
+	{
+		anim.SetTrigger ("Jab");		
+	}
+	public void strong ()
+	{
+		anim.SetTrigger ("Strong");
+	}
+	public void lose ()
+	{
+		anim.SetTrigger ("Lose");
+	}
+	public void guard ()
+	{
+		anim.SetTrigger ("Guard");
+	}
+	public void damage ()
+	{
+		anim.SetTrigger ("Damage");
+	}
 }
